5b6651ed62a6e4abdac1012a71ee9043434e3454
Yanbin Qiu <yanbin.qiu@morningstar.com>
2020-06-30 21:28:06 -0500

    first commit

---
+++ client/src/components/Ping.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <p>{{ msg }}</p>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'Ping',
+  data() {
+    return {
+      msg: 'Hello!',
+    };
+  },
+  methods: {
+    getMessage() {
+      const path = 'http://localhost:5000/ping'; // 连接到 Flask 的 /ping api
+      axios.get(path)
+        .then((res) => {
+          this.msg = res.data;
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    },
+  },
+  created() {
+    this.getMessage();
+  },
+};
+</script>
